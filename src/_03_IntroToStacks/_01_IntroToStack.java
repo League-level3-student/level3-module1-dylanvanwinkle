f0cf3bb577c71caea246eabbad8ff0fd9c647965
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-07-01 11:26:06 -0700

    UndoREdoIP

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,19 +9,32 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> ds = new Stack<Double>();
+		Random ran = new Random();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
-		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
-		//4. Pop all the elements off of the Stack. Every time a double is popped that is
+		for (int i = 0; i < 100; i++) {
+			double r = 101 * ran.nextDouble();
+			ds.push(r);
+
+		}
+		//3. Ask the user to enter in two numbers between 0 and 100, inclusive.
+		JOptionPane.showMessageDialog(null, "You wil give 2 numbers the first one should be smaller than the second \nyou can use decimal numbers."
+				+ " Make your numbers between 0 and 100 inclusive.");
+	String	a1 = JOptionPane.showInputDialog("Give a number.");
+	String	a2 = JOptionPane.showInputDialog("Give a second number.");
+	//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
-		
+		double an1 = Double.parseDouble(a1);
+		double an2 = Double.parseDouble(a2);
 		//   EXAMPLE:
 		//   NUM 1: 65
 		//   NUM 2: 75
-		
+		for (int i = 0; i < ds.size(); i++) {
+			double dsp = ds.pop();
+			if (dsp >= an1 && dsp <= an2  ) {
+				System.out.println(dsp);
+			}
+		}
 		//   Popping elements off stack...
 		//   Elements between 65 and 75:
 		//   66.66876846
@@ -31,5 +44,5 @@ public class _01_IntroToStack {
 		//   71.54506465
 		//   66.47984807
 		//   74.12121224
-	}
+	} 
 }
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,9 +1,13 @@
 package _03_IntroToStacks;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class _02_TextUndoRedo {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
+	 * J
 	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
 	 * 
 	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
@@ -13,6 +17,12 @@ public class _02_TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
+	JFrame f = new JFrame();
+	JPanel p = new JPanel();
+	JLabel l = new JLabel();
+	public static void main(String[] args) {
+		_02_TextUndoRedo tur = new _02_TextUndoRedo();
+		tur.p.add(tur.l);
+	}
 	
 }
